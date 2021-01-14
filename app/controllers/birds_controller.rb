67cf9644464aabacf5ef4c8bcb7fd44f70e136a3
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-01-14 10:19:01 -0700

    first commit

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,31 @@
+class BirdsController < ApplicationController
+    def index 
+        @birds = Bird.all 
+
+        render json: @birds
+    end
+
+    def show 
+        @bird = Bird.find(params[:id])
+        render json: @bird
+    end
+
+    def create 
+        @bird = Bird.create(name: params[:name])
+        render json: @bird
+    end
+
+    def update 
+        @bird = Bird.find(params[:id])
+        @bird.update(name: params[:name])
+
+        render json: @bird
+    end
+
+    def destroy 
+        @bird = Bird.find(params[:id])
+        @bird.destroy 
+
+        render json: "#{@bird.name} has been destroyed"
+    end
+end
